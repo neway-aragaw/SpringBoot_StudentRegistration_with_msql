2d39783c2847087a0058ee94a622a20442cc7f15
neway gebrie <niwaygebrie@gmail.com>
2023-05-10 16:59:56 -0400

    first commit

---
+++ src/main/java/com/example/emp/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author niway
+ *
+ */
+package com.example.emp;
---
+++ src/test/java/com/example/studentName/StudentNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentName;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
